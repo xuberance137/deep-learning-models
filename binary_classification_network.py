f7d44e9fc4a8d1b8f360e89600771b9fa5bd9377
Gopal Erinjippurath <gopale@captricity.com>
2017-04-10 10:53:41 -0700

    Adding test and prediction

--- binary_classification_network.py
+++ binary_classification_network.py
@@ -4,8 +4,12 @@ from keras.layers import Dense
 from keras.optimizers import RMSprop
 from keras.objectives import binary_crossentropy
 from keras.metrics import binary_accuracy
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 import numpy as np
+
+PLOT_TRAINING=0
 
 def vectorize_sequences(sequences, dimensions=10000):
 	results = np.zeros((len(sequences), dimensions))
@@ -13,6 +17,7 @@ def vectorize_sequences(sequences, dimensions=10000):
 		results[i, sequence] = 1
 	return results
 
+# get data
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(nb_words=10000)
 
 word_index = imdb.get_word_index()
@@ -22,23 +27,39 @@ for n in range(10):
 	decoded_review = ''.join([reverser_word_index.get(i-3, '*')+' ' for i in train_data[n]])
 	print decoded_review
 
+#vectorize/ ETL
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
-
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
-
-model = Sequential()
-model.add(Dense(16, activation='relu', input_dim=10000))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-
-model.compile(optimizer=RMSprop(lr=0.001), loss=binary_crossentropy, metrics=[binary_accuracy])
-
 x_val = x_train[:10000]
 partial_x_train = x_train[10000:]
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
+#build network
+# reset_selective model
+model = Sequential()
+model.add(Dense(16, activation='relu', input_dim=10000))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer=RMSprop(lr=0.001), loss=binary_crossentropy, metrics=[binary_accuracy])
+#train network
+history = model.fit(partial_x_train, partial_y_train, epochs=10, batch_size=512, validation_data=(x_val,y_val))
+if PLOT_TRAINING:
+	loss_values = history.history['loss']
+	val_loss_values = history.history['val_loss']
+	epochs = range(1, len(loss_values)+1)
+	plt.plot(epochs, loss_values, 'bo')
+	plt.plot(epochs, val_loss_values, 'b.')
+	plt.xlabel('Epochs')
+	plt.ylabel('Loss')
+	plt.show()
+#evaluate network
+results = model.evaluate(x_test, y_test)
+print 'Accuracy on test data set : ', results[1]
+#predict with network
+pred = model.predict(x_test)
 
-history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val,y_val))
+
+
 
